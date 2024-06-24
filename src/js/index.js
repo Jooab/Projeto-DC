599cc0291b3f92fb4881552657e19a715d6b82ff
Joab <joobascanal@gmail.com>
2024-06-24 12:23:11 -0300

    adicionando a guard clause para que o jogador 1 não possa ser o darkseid

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,14 @@ const CharactersList = document.querySelectorAll('.character')
 CharactersList.forEach(character => {
     character.addEventListener('mouseenter', () => {
 
+        const selectedId = character.attributes.id.value
+
+        if (selectedId === "darkseid") return;
+
         const selectedCharacter = document.querySelector('.selected')
         selectedCharacter.classList.remove('selected')
 
         character.classList.add('selected')
-
-        const selectedId = character.attributes.id.value
 
         const bigCharacter1Image = document.getElementById('player-1-character')
         bigCharacter1Image.src = `src/images/${selectedId}.png`
